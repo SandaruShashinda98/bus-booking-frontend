c15ed38f12220409bce205ca74cfbc7f384ef4d5
Sandaru-Shashinda <sandaru@zoomi.ca>
2025-03-24 17:12:22 +0530

    feat:add-more-pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import { StrictMode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
-import ProfilePage from "./pages/ProfilePage";
-import SingleProfilePage from "./pages/SingleProfilePage";
 // import Stats from "./pages/Stats";
 // import BusSeatSelection from "./pages/Booking";
 // import BookingCancellation from "./pages/Cancel";
@@ -19,6 +17,9 @@ import Dashboard from "./pages/Dashboard";
 import UserAccount from "./pages/user/UserAccount";
 import BusStaffManagement from "./pages/bus-staff/BusStaffManagement";
 import BusFleetManagement from "./pages/bus-fleet/BusFleetManagement";
+import RestaurantDashBoard from "./pages/RestaurantDashBoard";
+import MenuManagement from "./pages/restaurant/MenuManagement";
+import OrderManagement from "./pages/restaurant/OrderManagement";
 
 const router = createBrowserRouter([
   {
@@ -27,31 +28,21 @@ const router = createBrowserRouter([
     errorElement: <NotFoundPage />,
   },
   {
-    path: "/dashboard",
-    element: <Dashboard />,
-    errorElement: <NotFoundPage />,
-  },
-  {
     path: "/user-account",
     element: <UserAccount />,
+    errorElement: <NotFoundPage />,
+  },
+
+  // bus owner
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
     errorElement: <NotFoundPage />,
   },
   {
     path: "/bus-staff-management",
     element: <BusStaffManagement />,
     errorElement: <NotFoundPage />,
-  },
-  {
-    path: "/home",
-    element: <AvailableBusSchedules />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/seat-booking",
-    element: <SeatBooking />,
   },
   {
     path: "/trip-listing",
@@ -61,6 +52,37 @@ const router = createBrowserRouter([
     path: "/fleet-management",
     element: <BusFleetManagement />,
   },
+
+  // restaurant
+  {
+    path: "/restaurant-dashboard",
+    element: <RestaurantDashBoard />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/menu-management",
+    element: <MenuManagement />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/order-management",
+    element: <OrderManagement />,
+    errorElement: <NotFoundPage />,
+  },
+
+  // public
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/home",
+    element: <AvailableBusSchedules />,
+  },
+  {
+    path: "/seat-booking",
+    element: <SeatBooking />,
+  },
   {
     path: "/cancel-booking",
     element: <BookingCancellation />,
@@ -69,16 +91,16 @@ const router = createBrowserRouter([
     path: "/meal-pre-order",
     element: <MealPreOrder />,
   },
-  {
-    path: "/profile",
-    element: <ProfilePage />,
-    children: [
-      {
-        path: "/profile/:profileId",
-        element: <SingleProfilePage />,
-      },
-    ],
-  },
+  // {
+  //   path: "/profile",
+  //   element: <ProfilePage />,
+  //   children: [
+  //     {
+  //       path: "/profile/:profileId",
+  //       element: <SingleProfilePage />,
+  //     },
+  //   ],
+  // },
 ]);
 
 createRoot(document.getElementById("root")!).render(
--- src/pages/SingleProfilePage.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const SingleProfilePage = () => {
-  const params = useParams<{ profileId: string }>();
-  return <div>{params.profileId}</div>;
-};
-
-export default SingleProfilePage;
